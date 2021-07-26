f1dc14fb90a0048ac5c48a89424e8e91080d80e7
Andrej Noskaj <andrej.nosky@gmail.com>
2021-07-26 11:18:48 +0200

    Update example.bat

--- example.bat
+++ example.bat
@@ -1,1 +1,1 @@
-py src/analysis.py --vector_transition 1 --col "Finding Labels" --val "Hernia" --f_target 2 --f_steps 10 --samples 2
+py src/analysis.py --vector_transition --col "Finding Labels" --val "Hernia" --f_target 2 --f_steps 10 --samples 2
