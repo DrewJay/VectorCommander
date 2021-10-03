bdb3236517f97c1986bf6a58e6ff291c6832e80e
Andrej Noskaj <andrej.nosky@gmail.com>
2021-10-04 00:54:08 +0200

    Update VAE.py

--- src/models/VAE.py
+++ src/models/VAE.py
@@ -38,7 +38,7 @@ class Sampling(Layer):
         delta = rest[0] if len(rest) >= 1 else 1
 
         epsilon = delta * K.random_normal(shape=K.shape(mu), mean=0., stddev=1.)
-        # Square root of variance. Because aX + b where X is standard normal random vector, a increases variance a² times.
+        # Square root of variance. Because in aX + b where X is standard normal random vector, a increases variance a² times.
         return mu + K.exp(log_var / 2) * epsilon
 
 
